7e84206b6bb9f2b003db110dd4395714b931338e
Tom Rosenbloom <TomRosenbloom@users.noreply.github.com>
2018-05-21 11:52:46 +0100

    added zurb assets and working css link in header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title><?php echo $title; ?></title>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>zurb/css/foundation.min.css">
     </head>
     <body>
 
