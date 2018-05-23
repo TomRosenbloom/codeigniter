f8b60a1d2aca1c72a572c1444b7e35b775ccd59d
Tom Rosenbloom <TomRosenbloom@users.noreply.github.com>
2018-05-23 13:49:21 +0100

    improved layout of show view

--- application/views/contacts/show.php
+++ application/views/contacts/show.php
@@ -1,10 +1,20 @@
-<?php
-echo "<h2>" . $contact['first_name'] . " " . $contact['last_name'] . "</h2>";
-echo $contact['email'];
-?>
-<p>
-<a href="<?= site_url('edit/' . $contact['slug']) ?>">Edit</a>
-</p>
-<?php echo form_open('delete/' . $contact['id'], array('onsubmit' => "return confirm('Really delete?')")); ?>
-    <input type="submit" value="delete">
-</form>
+<div class="grid-x grid-padding-x">
+  <div class="cell">
+      <h2><?= $contact['first_name'] . " " . $contact['last_name'] ?></h2>
+  </div>
+  <div class="cell small-2">
+      Email address:
+  </div>
+  <div class="cell small-4">
+      <?= $contact['email'] ?>
+  </div>
+  <div class="cell">
+      &nbsp;
+  </div>
+  <div class="cell">
+      <button type="button" class="success button"><a href="<?= site_url('edit/' . $contact['slug']) ?>">Edit</a></button>
+      <?php echo form_open('delete/' . $contact['id'], array('onsubmit' => "return confirm('Really delete?')", 'style' => "display: inline")); ?>
+          <input type="submit" value="delete" class="alert button">
+      </form>
+  </div>
+</div>
