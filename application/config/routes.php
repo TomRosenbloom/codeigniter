79ca699b15c024f9e3cd4f2931edf5c10de1e646
Tom Rosenbloom <TomRosenbloom@users.noreply.github.com>
2018-05-28 08:31:03 +0100

    authentication with ion-auth

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,6 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = 'contacts';
 
+$route['dashboard'] = 'dashboard/index';
+
 $route['(:num)'] = 'contacts/index/$1';
 $route['contacts'] = 'contacts/index';
 
